31fb27b4b02edc414214365fb20a5bdc28d6a637
Volodymyr <Volodumurvel@gmail.com>
2025-11-05 21:16:35 +0100

    feat: create constants for limit

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -6,3 +6,6 @@ const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
 
 export { APP_NAME, APP_DESCRIPTION, SERVER_URL };
+
+export const LATEST_PRODUCTS_LIMIT =
+  process.env.NEXT_PUBLIC_LATEST_PRODUCTS_LIMIT || "3";
